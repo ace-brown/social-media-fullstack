6b01c4d67540d61eb175e3904579567e133dbcce
ace-brown <kazem_abdi@yahoo.com>
2023-08-12 15:17:06 +0200

    add: Feed.js

---
+++ client/src/pages/Credits.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CreditsPage = () => {
+  return (
+    <div>
+      <p>
+        Photo by{" "}
+        <a href="https://unsplash.com/@framesforyourheart?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Frames For Your Heart
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/mR1CIDduGLc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default CreditsPage;
--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imgUrl: "images/frames-for-your-heart.jpg",
+        creator: {
+          name: "John",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -9,7 +20,13 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: "John",
+      createdAt: new Date(),
+    },
   });
 };
